e64e186f7d0706115c869f036f6c840e7753a2c9
Beatriz Rabelo <beatriz.devrabelo@gmail.com>
2022-08-31 23:56:42 -0300

    Convertendo string para o tipo DateTime

---
+++ ManipulandoDatasHumanizer/CalculoDaDiferençaData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulandoDatasHumanizer
+{
+    class CalculoDaDiferençaData
+    {
+        public CalculoDaDiferençaData(int ano, int mes, int dia)
+        {
+            string dataString = Convert.ToString(ano) + "-" + Convert.ToString(mes) + "-" + Convert.ToString(dia);
+
+            DateTime dataDateX = DateTime.Parse(dataString);
+
+            string mensagem = "data: " + dataString;
+
+            Console.WriteLine(mensagem);
+        }
+    }
+}
---
+++ ManipulandoDatasHumanizer/Gerenciador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulandoDatasHumanizer
+{
+    class Gerenciador
+    {
+        public static void Main(string[] args)
+        {
+            CalculoDaDiferençaData calculo = new CalculoDaDiferençaData(2022, 9, 15);
+        }
+    }
+}
